ede3b215690637f1b499da503782adb3de110846
Vicente Ramirez Perea <vicente.ramirez.perea@gmail.com>
2016-11-27 13:31:42 +0100

    Leer estructura de config.yaml

---
+++ jinjatex.py
@@ -0,0 +1,44 @@
+from __future__ import print_function
+import re
+import jinja2
+
+# Example template
+# Your LaTeX template might look like this:
+# \documentclass{article}
+#
+# \begin{document}
+# Hello, ((( name|escape_tex )))!
+# \end{document}
+# This snippet by Clemens Kaposi can be used freely for anything you like. Consider it public domain.
+
+# Partialy extracted (changing template syntax and escape_tex) from http://flask.pocoo.org/snippets/55/
+LATEX_SUBS = (
+    (re.compile(r'\\'), r'\\textbackslash'),
+    (re.compile(r'([{}_#%&$])'), r'\\\1'),
+    (re.compile(r'~'), r'\~{}'),
+    (re.compile(r'\^'), r'\^{}'),
+    (re.compile(r'"'), r"''"),
+    (re.compile(r'\.\.\.+'), r'\\ldots'),
+)
+
+def escape_tex(value):
+    newval = value
+    for pattern, replacement in LATEX_SUBS:
+        newval = pattern.sub(replacement, newval)
+    return newval
+
+
+
+def render_tex(template_filename, doc):
+    # make new syntax
+    texenv = jinja2.create_jinja_environment()
+    texenv.block_start_string = '((*'
+    texenv.block_end_string = '*))'
+    texenv.variable_start_string = '((('
+    texenv.variable_end_string = ')))'
+    texenv.comment_start_string = '((='
+    texenv.comment_end_string = '=))'
+    texenv.filters['escape_tex'] = escape_tex
+    # Render
+    template = texenv.get_template(template_filename)
+    template.render(doc)
--- mcv.py
+++ mcv.py
@@ -1,40 +1,42 @@
-# Partialy extracted (changing template syntax and escape_tex) from http://flask.pocoo.org/snippets/55/
 
-LATEX_SUBS = (
-    (re.compile(r'\\'), r'\\textbackslash'),
-    (re.compile(r'([{}_#%&$])'), r'\\\1'),
-    (re.compile(r'~'), r'\~{}'),
-    (re.compile(r'\^'), r'\^{}'),
-    (re.compile(r'"'), r"''"),
-    (re.compile(r'\.\.\.+'), r'\\ldots'),
-)
+from __future__ import print_function
+import os
+import argparse
+import yaml
+import jinjatex
+import datetime
 
-def escape_tex(value):
-    newval = value
-    for pattern, replacement in LATEX_SUBS:
-        newval = pattern.sub(replacement, newval)
-    return newval
-
-texenv = app.create_jinja_environment()
-texenv.block_start_string = '((*'
-texenv.block_end_string = '*))'
-texenv.variable_start_string = '((('
-texenv.variable_end_string = ')))'
-texenv.comment_start_string = '((='
-texenv.comment_end_string = '=))'
-texenv.filters['escape_tex'] = escape_tex
-
-# Render
-template = texenv.get_template('template.tex')
-template.render(name='Tom')
+cml = argparse.ArgumentParser(
+    description="This program genrate a .tex or .html file of a Curriculum Vitae using templates mades with jinja "
+                "and data stored as .yaml. To see an example and tutorial see "
+                "http://victe.github.com/cv-templating",
+    formatter_class=argparse.RawTextHelpFormatter)
+cml.add_argument("config_file", help="File .yaml with specifications of the data and template to use. You must have "
+                                "one of this config file for each combination of format, language or data "
+                                "contents. You have various examples of config files in the directory examples.")
 
 
 
-# Example template
-# Your LaTeX template might look like this:
-# \documentclass{article}
-#
-# \begin{document}
-# Hello, ((( name|escape_tex )))!
-# \end{document}
-# This snippet by Clemens Kaposi can be used freely for anything you like. Consider it public domain.
+if __name__ == "__main__":
+    args=cml.parse_args()
+    config = yaml.load(open(args.config_file, 'r'))
+    cf=config['config']
+    lang='.'+cf['language']
+    output_dir= os.path.join(cf['base_dir'],cf['output_dir']) \
+        if cf['output_dir'] != '' or cf['output_dir'] is None \
+        else cf['base_dir']
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+    doc = config['doc']
+    # Cargar info de yaml de datos adicionales For each entry in dict doc
+    for d in doc:
+        print(d, doc.get(d))
+        if exist file para cada key, sustituir key with the info in file
+
+
+
+    ext = 'tex' if cf['type'] == 'latex' else 'html'
+    outputfile=os.path.join(config.doc.pers_info.name + datetime.datetime.now() + '.' + ext)
+
+    with open(outputfile, 'w') as o:
+        o.write(jinjatex.render_tex(config.config.template, doc=doc))
