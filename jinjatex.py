07082e35a401dc1f411387bce01747070da89499
Vicente Ramirez Perea <vicente.ramirez.perea@gmail.com>
2016-11-29 19:29:23 +0100

    Revert "Errores no funciona para inmediato revert"
    This reverts commit 3dde86aca4d6255d69e0957591583b32b29f9213.

--- jinjatex.py
+++ jinjatex.py
@@ -22,7 +22,6 @@ LATEX_SUBS = (
 )
 
 def escape_tex(value):
-    print(value)
     newval = value
     for pattern, replacement in LATEX_SUBS:
         newval = pattern.sub(replacement, newval)
--- mcv_utils.py
+++ mcv_utils.py
@@ -18,14 +18,3 @@ def merge_two_dicts(x, y):
     z = x.copy()
     z.update(y)
     return z
-
-def merge_dicts(*dict_args):
-    '''
-    Given any number of dicts, shallow copy and merge into a new dict,
-    precedence goes to key value pairs in latter dicts.
-    http://stackoverflow.com/a/26853961
-    '''
-    result = {}
-    for dictionary in dict_args:
-        result.update(dictionary)
-    return result
