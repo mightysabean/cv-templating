98d94c8d1d868e1c0c4185ae28ff17466abf60a2
Vicente Ramirez Perea <vicente.ramirez.perea@gmail.com>
2017-02-08 17:41:14 +0100

    Limpiar un poco mcv pylint

--- mcv.py
+++ mcv.py
@@ -2,13 +2,14 @@
 from __future__ import print_function
 import os
 import argparse
-import datetime
 import yaml
-import jinjatex
-import mcv_utils
 import mcv_c
 
+
 def create_parser():
+    """Create command line parser using argparse
+    @return object argparse, list of arguments (now only name of config file)
+    """
     cml = argparse.ArgumentParser(
         description="This program generate a .tex or .html file of a Curriculum"
                     " Vitae using templates made with jinja "
@@ -26,15 +27,20 @@ def create_parser():
     return cml
 
 
-def main(configFile):
-    if not os.path.exists(configFile):
-        raise RuntimeError('Can not be found the file "%s" . The path must be relative from where you are "'
-                           'executing the program, as well a full path.' % configFile)
-    config = yaml.load(open(configFile, 'r'))
+def main(config_file):
+    """ Main function load a config file with the info of what template and
+    where find it, and the data to pass to it.
+    @return nothing, generates file.
+    """
+    if not os.path.exists(config_file):
+        raise RuntimeError('Can not be found the file "%s" . The path must be '
+                           'relative from where you are '
+                           'executing the program, as well a full path.' % config_file)
+    config = yaml.load(open(config_file, 'r'))
     cvg = mcv_c.CVGenerator(config)
     cvg.render()
 
 
 if __name__ == "__main__":
-    args = create_parser().parse_args()
-    main(args.config_file)
+    CML_ARGS = create_parser().parse_args()
+    main(CML_ARGS.config_file)
