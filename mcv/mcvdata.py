4daf6aa2229a287adbfe11e997bbbfbfdc20da36
Vicente Ramirez Perea <vicente.ramirez.perea@gmail.com>
2017-02-11 11:04:03 +0100

    Implement csv2dict

--- mcv/mcvdata.py
+++ mcv/mcvdata.py
@@ -1,5 +1,8 @@
+# -*- coding: utf-8 -*-
+import csv
 import os
 
+import sys
 import yaml
 
 from mcv.mcvutils import fileext
@@ -35,7 +38,7 @@ class CVData():
                 self.__data_files.get(d)
             )
             # Add otros formatos de archivo si se necesita.
-            if fileext(f) == 'csv':
+            if fileext(f) == '.csv':
                 datadictsinglefile = {d: self.__csv2dict(f)}
             else:
                 datadictsinglefile = {d: yaml.load(open(f, 'r'))}
@@ -46,5 +49,26 @@ class CVData():
     def __csv2dict(self, f):
         """Convert data in csv format to YAML list.
 
-        No se hacen comprobaciones de correccion del csv"""
+        No se hacen comprobaciones de correccion del csv
 
+        El csv debe tener header con nombres que vayan a ser compatibles con variables
+        en el template de jinja2. Lo que se hace es una traduccion de cada fila a una
+        entrada de diccionario con keys los nombres de columna y value el de las celdas."""
+
+        listRows = []
+        if (sys.version_info[0] > 2):
+            kw = {'delimiter': ','}
+        else:
+            kw = {'delimiter': ',', 'encoding': 'utf-8'}
+        rows = csv.reader(f, **kw)
+        for i, row in enumerate(rows):
+
+            if (i == 0):
+                headers = row
+            else:
+                listRows.append(row)
+
+        listData = []
+        for row in listRows:
+            listData.append(dict(zip(headers, row)))
+        return listData
