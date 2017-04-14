d3e7a1f16babf9e9c221101eaa0a07bd41eea63f
Vicente Ramirez Perea <vicente.ramirez.perea@gmail.com>
2017-04-14 03:49:55 +0200

    Error enlaces

--- release.py
+++ release.py
@@ -1,4 +1,7 @@
 from distutils.dir_util import copy_tree
-
+from shutil import copyfile
 copy_tree('sphinx/_build/html','docs')
 
+copyfile('sphinx/Someone.html','docs/Someone.html')
+copyfile('sphinx/Someone.pdf','docs/Someone.pdf')
+copy_tree('sphinx/images','docs/images')
